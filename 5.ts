348e44103222fcbc219147fee68b06835eba0d76
Peter Groß <pgross@newline.studio>
2021-12-06 15:38:15 +0100

    day 5b

--- 5.ts
+++ 5.ts
@@ -20,6 +20,9 @@ const vents: VentMap = {
   rows: [],
 };
 
+let maxX = 0;
+let maxY = 0;
+
 for (const pipe of pipes) {
   const source = pipe[0].split(',').map((el) => parseInt(el));
   const target = pipe[1].split(',').map((el) => parseInt(el));
@@ -29,24 +32,52 @@ for (const pipe of pipes) {
   const targetX = target[0];
   const targetY = target[1];
 
+  maxX = Math.max(maxX, sourceX, targetX);
+  maxY = Math.max(maxY, sourceY, targetY);
+}
+
+for (let y = 0; y <= maxY; y++) {
+  vents.rows[y] = {
+    tiles: [],
+  }
+}
+
+for (let x = 0; x <= maxX; x++) {
+  for (let y = 0; y <= maxY; y++) {
+    vents.rows[y].tiles[x] = {
+      visited: 0,
+    }
+  }
+}
+
+for (const pipe of pipes) {
+  const source = pipe[0].split(',').map((el) => parseInt(el));
+  const target = pipe[1].split(',').map((el) => parseInt(el));
+
+  let sourceX = source[0];
+  let sourceY = source[1];
+  const targetX = target[0];
+  const targetY = target[1];
+
+  // diagonal lines (45 degrees)
+  if (Math.abs(sourceX - targetX) === Math.abs(sourceY - targetY)) {
+    let delta = Math.abs(targetX - sourceX);
+    let modX = targetX >= sourceX ? 1 : -1;
+    let modY = targetY >= sourceY ? 1 : -1;
+
+    for (let i = delta; delta >= 0; delta--) {
+      vents.rows[sourceY].tiles[sourceX].visited++;
+      sourceX += modX;
+      sourceY += modY;
+    }
+  }
+
   // vertical lines
   if (sourceX === targetX) {
     let from = Math.min(sourceY, targetY);
     let to = Math.max(sourceY, targetY);
 
     for (from; from <= to; from++) {
-      if (!vents.rows[from]) {
-        vents.rows[from] = {
-          tiles: [],
-        }
-      }
-
-      if (!vents.rows[from].tiles[sourceX]) {
-        vents.rows[from].tiles[sourceX] = {
-          visited: 0,
-        }
-      }
-
       vents.rows[from].tiles[sourceX].visited++;
     }
   }
@@ -56,55 +87,17 @@ for (const pipe of pipes) {
     let from = Math.min(sourceX, targetX);
     let to = Math.max(sourceX, targetX);
 
-    if (!vents.rows[sourceY]) {
-      vents.rows[sourceY] = {
-        tiles: [],
-      }
-    }
-
     for (from; from <= to; from++) {
-      if (!vents.rows[sourceY].tiles[from]) {
-        vents.rows[sourceY].tiles[from] = {
-          visited: 0,
-        }
-      }
-
       vents.rows[sourceY].tiles[from].visited++;
-    }
-  }
-
-  // diagonal lines
-  if ((sourceX === sourceY && targetX === targetY) || (sourceX === targetY && sourceY === targetX)) {
-    let fromX = Math.min(sourceX, targetX);
-    let toX = Math.max(sourceX, targetX);
-    let fromY = Math.min(sourceY, targetY);
-    let toY = Math.max(sourceY, targetY);
-
-    for (fromY; fromY <= toY; fromY++) {
-      if (!vents.rows[fromY]) {
-        vents.rows[fromY] = {
-          tiles: [],
-        }
-      }
-
-      for (fromX; fromX <= toX; fromX++) {
-        if (!vents.rows[sourceY].tiles[fromX]) {
-          vents.rows[sourceY].tiles[fromX] = {
-            visited: 0,
-          }
-        }
-
-        vents.rows[sourceY].tiles[fromX].visited++;
-      }
     }
   }
 }
 
 let overlaps = 0;
 for (const row of vents.rows) {
-  if (!row?.tiles) continue;
+  if (!row.tiles) continue;
   for (const tile of row.tiles) {
-    if (tile && tile.visited > 1) {
+    if (tile.visited > 1) {
       overlaps++;
     }
   }
